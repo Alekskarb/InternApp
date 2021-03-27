d648605f7cb1f06b1623a533bf7f85867424de25
Alekskarb <korbutowski@gmail.com>
2021-03-27 17:00:48 +0300

    create homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { RegformComponent } from './regform/regform.component';
+import { TraindescrComponent } from './traindescr/traindescr.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    RegformComponent,
+    TraindescrComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/traindescr/traindescr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-traindescr',
+  templateUrl: './traindescr.component.html',
+  styleUrls: ['./traindescr.component.scss']
+})
+export class TraindescrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
