678f9ffa83a250e721ec4f94109ef99892fa7ea7
Alekskarb <korbutowski@gmail.com>
2021-03-28 20:33:43 +0300

    fix: modules components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomepageComponent} from './homepage/homepage.component';
-import {RegformComponent} from './regform/regform.component';
-import {TraindescrComponent} from './traindescr/traindescr.component';
 
-const routes: Routes = [
-  {path: '', component: HomepageComponent},
-  // {path: 'regform', component: RegformComponent},
-  // {path: 'traindescr', component: TraindescrComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { RegformComponent } from './regform/regform.component';
-import { TraindescrComponent } from './traindescr/traindescr.component';
 import {HomepageModule} from './homepage/homepage.module';
 import {RegformModule} from './regform/regform.module';
 import {TraindescrModule} from './traindescr/traindescr.module';
@@ -13,9 +10,6 @@ import {TraindescrModule} from './traindescr/traindescr.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    RegformComponent,
-    TraindescrComponent
   ],
   imports: [
     BrowserModule,
